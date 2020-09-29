4697f00144fa6bc95f48c6cad83cccc1b0f8e898
BeomJin An <kingkong3502@gmail.com>
2020-09-29 17:23:25 +0900

    QR체크인 + 쿠팡광고 프로젝트 업데이트

---
+++ p200929_qrcheckin/android/app/src/main/kotlin/com/example/p200929_qrcheckin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p200929_qrcheckin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ p200929_qrcheckin/lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_coupang_ad/flutter_coupang_ad.dart';
+
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: WebViewExample()));
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Stack(
+            children: [
+              Builder(
+                builder: (BuildContext context) {
+                  return WebView(
+                    initialUrl: 'https://nid.naver.com/login/privacyQR',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController) {
+                      _controller.complete(webViewController);
+                    },
+                  );
+                },
+              ),
+              Align(
+                  alignment: Alignment.bottomCenter,
+                  child: CoupangAdView(
+                      CoupangAdConfig(adId: '332365', height: 100)))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
